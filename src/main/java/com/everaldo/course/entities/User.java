98f7a05abc3ade45c5fa477e6cbb8029acda53d7
Everaldo Faustino <everaldo_faustino@yahoo.com.br>
2026-05-22 20:11:24 -0300

    H2 database, test profile, JPA

--- src/main/java/com/everaldo/course/entities/User.java
+++ src/main/java/com/everaldo/course/entities/User.java
@@ -1,13 +1,20 @@
 package com.everaldo.course.entities;
 
+import jakarta.persistence.*;
+import jdk.jfr.Enabled;
+
 import java.io.Serial;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
