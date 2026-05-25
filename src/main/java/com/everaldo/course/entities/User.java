9e4fb713e5168b9603a907567a879c5ce4353db5
Everaldo Faustino <everaldo_faustino@yahoo.com.br>
2026-05-25 17:20:29 -0300

    Order, Instant, ISO 8601 part-1

--- src/main/java/com/everaldo/course/entities/User.java
+++ src/main/java/com/everaldo/course/entities/User.java
@@ -5,6 +5,8 @@ import jdk.jfr.Enabled;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -20,6 +22,11 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>(); // ← Associação
+
+
 
     public User(){
 
@@ -73,6 +80,10 @@ public class User implements Serializable {
         this.password = password;
     }
 
+    public List<Order> getOrders() {
+        return orders;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
@@ -84,6 +95,9 @@ public class User implements Serializable {
     public int hashCode() {
         return Objects.hashCode(id);
     }
+
+
+
 
 
 }
