03ec094d8823fa1799eb5cbf54d538ff7ca788e8
Everaldo Faustino <everaldo_faustino@yahoo.com.br>
2026-05-27 11:17:10 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/everaldo/course/entities/Category.java
+++ src/main/java/com/everaldo/course/entities/Category.java
@@ -1,5 +1,6 @@
 package com.everaldo.course.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Setter;
 
@@ -21,7 +22,8 @@ public class Category implements Serializable {
     @Setter
     private String name;
 
-    @Transient
+    @JsonIgnore
+    @ManyToMany(mappedBy = "categories")
     private Set<Product> products = new HashSet<>();
 
     public Category(){
